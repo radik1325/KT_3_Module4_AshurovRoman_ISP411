b5414d34c25f3324960f3f3f76d6090b0f882680
Коммерческие курсы <comcuru@teach.local>
2024-10-10 14:46:14 +0300

    firstcomm

---
+++ AshurovRoman_DataUser/Classes/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace AshurovRoman_DataUser.Classes
+{
+    class Manager
+    {
+        public static Frame MainFrame { get; set; }
+    }
+}
---
+++ AshurovRoman_DataUser/Pages/ListViewUser.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AshurovRoman_DataUser.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ListViewUser.xaml
+    /// </summary>
+    public partial class ListViewUser : Page
+    {
+        
+        public ListViewUser()
+        {
+            InitializeComponent();
+            ListUser.ItemsSource = Data.DataUserEntities.GetContext().User.ToList();
+        }
+
+        private void AddUser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
